4ca0ae49bd6f070c766a12373b0ef7f86e2c6683
Nacho Caiafa <ignacio.caiafa@gmail.com>
2021-08-07 12:19:35 -0300

    add client routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react"
-import { ChakraProvider, Flex, Text, Link } from "@chakra-ui/react"
+import { ChakraProvider, Flex, Text } from "@chakra-ui/react"
 import { useDispatch } from "react-redux"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 import theme from "./theme"
 import Posts from "./components/Posts/Posts"
 import Form from "./components/Form/Form"
 import { getPosts } from "./redux/posts"
+import Test from "./components/Test"
 
 const App = () => {
 	const dispatch = useDispatch()
@@ -17,23 +19,34 @@ const App = () => {
 
 	return (
 		<ChakraProvider theme={theme}>
-			<Flex direction="column" p="8" w="100%">
-				<Flex marginBottom="8" w="100%">
+			<Router>
+				<Flex paddingTop="8" paddingX="8" w="100%">
 					<Text color="primary.500" fontSize="xl">
-						<Link href="/">Forito</Link>
+						<Link to="/">Forito</Link>
+						<Link to="/test">Test</Link>
 					</Text>
 				</Flex>
 
-				<Flex direction={{ sm: "column", md: "column", lg: "row", xl: "row" }} w="100%">
-					<Flex w="100%">
-						<Posts setCurrentId={setCurrentId} />
-					</Flex>
+				<Switch>
+					<Route path="/test">
+						<Test />
+					</Route>
 
-					<Flex>
-						<Form currentId={currentId} setCurrentId={setCurrentId} />
-					</Flex>
-				</Flex>
-			</Flex>
+					<Route path="/">
+						<Flex direction="column" p="8" w="100%">
+							<Flex direction={{ sm: "column", md: "column", lg: "row", xl: "row" }} w="100%">
+								<Flex w="100%">
+									<Posts setCurrentId={setCurrentId} />
+								</Flex>
+
+								<Flex>
+									<Form currentId={currentId} setCurrentId={setCurrentId} />
+								</Flex>
+							</Flex>
+						</Flex>
+					</Route>
+				</Switch>
+			</Router>
 		</ChakraProvider>
 	)
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 
+import { store } from "./redux/store"
 import "./index.css"
 import App from "./App"
-import { store } from "./redux/store"
 
 ReactDOM.render(
-	<Provider store={store}>
-		<React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
 			<App />
-		</React.StrictMode>
-	</Provider>,
+		</Provider>
+	</React.StrictMode>,
 	document.getElementById("root")
 )
