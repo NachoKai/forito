269c8b6a84697a7245db2c22e0e4d97967651871
Nacho Caiafa <ignacio.caiafa@gmail.com>
2021-08-07 21:06:52 -0300

    add eslint unused vars rule

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ const CONNECTION_URL = process.env.MONGODB_URI;
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
 app.use("/posts", postRoutes);
 
 app.get("/", (req, res) => {
@@ -23,7 +22,7 @@ mongoose
 	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() =>
 		app.listen(PORT, () => {
-			console.log(`Listening on port ${PORT}`);
+			console.log(`Forito listening on port ${PORT}`);
 		})
 	)
 	.catch(err => {
