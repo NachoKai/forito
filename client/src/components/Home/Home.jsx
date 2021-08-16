89af5321e3d356cedcab2c076ba7fbce8019a860
Nacho Caiafa <ignacio.caiafa@gmail.com>
2021-08-15 23:12:50 -0300

    add pagination and search backend

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,22 +1,50 @@
-import { useState } from "react"
-import { Flex, Stack } from "@chakra-ui/react"
-import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { Button, Flex, Stack } from "@chakra-ui/react"
+import { useHistory, useLocation } from "react-router-dom"
+import ChipInput from "material-ui-chip-input"
+import styled from "styled-components"
 
 import Posts from "../Posts/Posts"
 import Form from "../Form/Form"
+import { getPosts, getPostsBySearch } from "../../redux/posts"
 import Pagination from "../Paginate/Pagination"
-import Search from "./Search"
+import FormInput from "../common/FormInput"
 
 function useQuery() {
 	return new URLSearchParams(useLocation().search)
 }
 
 const Home = () => {
+	const dispatch = useDispatch()
 	const [currentId, setCurrentId] = useState(0)
 	const query = useQuery()
+	const history = useHistory()
 	const page = query.get("page") || 1
 	const searchQuery = query.get("searchQuery")
+	const [searchValue, setSearchValue] = useState("")
 	const [searchTags, setSearchTags] = useState([])
+
+	const searchPost = () => {
+		if (searchValue.trim() || searchTags) {
+			dispatch(getPostsBySearch({ search: searchValue, tags: searchTags.join(",") }))
+			history.push(
+				`/posts/search?searchQuery=${searchValue || "none"}&tags=${searchTags.join(",")}`
+			)
+		} else {
+			history.push("/")
+		}
+	}
+
+	const handleKeyPress = e => {
+		if (e.keyCode === 188 || e.keyCode === 13) {
+			searchPost()
+		}
+	}
+
+	const handleAddTag = tag => setSearchTags([...searchTags, tag])
+	const handleDeleteTag = tagToDelete =>
+		setSearchTags(searchTags.filter(tag => tag !== tagToDelete))
 
 	return (
 		<Stack
@@ -27,18 +55,33 @@ const Home = () => {
 			<Stack w="100%">
 				<Posts setCurrentId={setCurrentId} />
 			</Stack>
-			<Stack>
-				<Search />
-				<Form currentId={currentId} setCurrentId={setCurrentId} />
 
+			<Stack>
+				<FormInput
+					label="Search Posts"
+					maxLength="105"
+					name="search"
+					value={searchValue}
+					onChange={e => {
+						setSearchValue(e.target.value)
+					}}
+					onKeyPress={handleKeyPress}
+				/>
+				<TagsContainer>
+					<ChipInput
+						label="Search Tags"
+						value={searchTags}
+						onAdd={tag => handleAddTag(tag)}
+						onDelete={tag => handleDeleteTag(tag)}
+					/>
+				</TagsContainer>
+				<Button colorScheme="primary" onClick={searchPost}>
+					Search
+				</Button>
+
+				<Form currentId={currentId} setCurrentId={setCurrentId} />
 				<Flex>
-					{!searchQuery && !searchTags.length && (
-						<Pagination
-							page={page}
-							searchTags={searchTags}
-							setSearchTags={setSearchTags}
-						/>
-					)}
+					<Pagination page={page} />
 				</Flex>
 			</Stack>
 		</Stack>
@@ -46,3 +89,8 @@ const Home = () => {
 }
 
 export default Home
+
+const TagsContainer = styled(Flex)`
+	background: white;
+	color: black;
+`
--- client/src/components/Home/Search.jsx
+++ client/src/components/Home/Search.jsx
@@ -1,69 +1,69 @@
-import { useState } from "react"
-import { useDispatch } from "react-redux"
-import { Button, Flex } from "@chakra-ui/react"
-import { useHistory } from "react-router-dom"
-import ChipInput from "material-ui-chip-input"
-import styled from "styled-components"
+// import { useState } from "react"
+// import { useDispatch } from "react-redux"
+// import { Button, Flex } from "@chakra-ui/react"
+// import { useHistory } from "react-router-dom"
+// import ChipInput from "material-ui-chip-input"
+// import styled from "styled-components"
 
-import { getPostsBySearch } from "../../redux/posts"
-import FormInput from "../common/FormInput"
+// import { getPostsBySearch } from "../../redux/posts"
+// import FormInput from "../common/FormInput"
 
-const Search = ({ searchTags, setSearchTags }) => {
-	const dispatch = useDispatch()
-	const history = useHistory()
-	const [searchValue, setSearchValue] = useState("")
+// const Search = ({ searchTags, setSearchTags }) => {
+// 	const dispatch = useDispatch()
+// 	const history = useHistory()
+// 	const [searchValue, setSearchValue] = useState("")
 
-	const searchPost = () => {
-		if (searchValue.trim() || searchTags) {
-			dispatch(getPostsBySearch({ search: searchValue, tags: searchTags.join(",") }))
-			history.push(
-				`/posts/search?searchQuery=${searchValue || "none"}&tags=${searchTags.join(",")}`
-			)
-		} else {
-			history.push("/")
-		}
-	}
+// 	const searchPost = () => {
+// 		if (searchValue.trim() || searchTags) {
+// 			dispatch(getPostsBySearch({ search: searchValue, tags: searchTags.join(",") }))
+// 			history.push(
+// 				`/posts/search?searchQuery=${searchValue || "none"}&tags=${searchTags.join(",")}`
+// 			)
+// 		} else {
+// 			history.push("/")
+// 		}
+// 	}
 
-	const handleKeyPress = e => {
-		if (e.keyCode === 188 || e.keyCode === 13) {
-			searchPost()
-		}
-	}
+// 	const handleKeyPress = e => {
+// 		if (e.keyCode === 188 || e.keyCode === 13) {
+// 			searchPost()
+// 		}
+// 	}
 
-	const handleAddTag = tag => setSearchTags([...searchTags, tag])
-	const handleDeleteTag = tagToDelete =>
-		setSearchTags(searchTags.filter(tag => tag !== tagToDelete))
+// 	const handleAddTag = tag => setSearchTags([...searchTags, tag])
+// 	const handleDeleteTag = tagToDelete =>
+// 		setSearchTags(searchTags.filter(tag => tag !== tagToDelete))
 
-	return (
-		<>
-			<FormInput
-				label="Search Posts"
-				maxLength="105"
-				name="search"
-				value={searchValue}
-				onChange={e => {
-					setSearchValue(e.target.value)
-				}}
-				onKeyPress={handleKeyPress}
-			/>
-			<TagsContainer>
-				<ChipInput
-					label="Search Tags"
-					value={searchTags}
-					onAdd={tag => handleAddTag(tag)}
-					onDelete={tag => handleDeleteTag(tag)}
-				/>
-			</TagsContainer>
-			<Button colorScheme="primary" onClick={searchPost}>
-				Search
-			</Button>
-		</>
-	)
-}
+// 	return (
+// 		<>
+// 			<FormInput
+// 				label="Search Posts"
+// 				maxLength="105"
+// 				name="search"
+// 				value={searchValue}
+// 				onChange={e => {
+// 					setSearchValue(e.target.value)
+// 				}}
+// 				onKeyPress={handleKeyPress}
+// 			/>
+// 			<TagsContainer>
+// 				<ChipInput
+// 					label="Search Tags"
+// 					value={searchTags}
+// 					onAdd={tag => handleAddTag(tag)}
+// 					onDelete={tag => handleDeleteTag(tag)}
+// 				/>
+// 			</TagsContainer>
+// 			<Button colorScheme="primary" onClick={searchPost}>
+// 				Search
+// 			</Button>
+// 		</>
+// 	)
+// }
 
-export default Search
+// export default Search
 
-const TagsContainer = styled(Flex)`
-	background: white;
-	color: black;
-`
+// const TagsContainer = styled(Flex)`
+// 	background: white;
+// 	color: black;
+// `
