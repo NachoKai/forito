caf1e1d44a32986c33307cb6dbd48f2ab37798f1
Nacho Caiafa <ignacio.caiafa@gmail.com>
2021-08-17 20:58:25 -0300

    refactor post styles

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -26,24 +26,22 @@ const Post = ({
 	post: { _id, title, name, creator, message, likes, createdAt, tags, selectedFile },
 }) => {
 	const dispatch = useDispatch()
+	const history = useHistory()
+	const user = getUser()
 	const bg = useColorModeValue("primary.50", "primary.800")
 	const bgDelete = useColorModeValue("red.500", "red.200")
-	const user = getUser()
-	const userId = user?.result.googleId || user?.result?._id
+	const userId = user?.result?.googleId || user?.result?._id
+	const isUserLike = likes?.find(like => like === userId)
+	const [isDialogOpen, setIsDialogOpen] = useState(false)
+	const [likesMock, setLikesMock] = useState(likes)
 	const isPostCreator =
 		user?.result?.googleId === creator || user?.result?._id === creator
-	const isUserLike =
-		likes && likes.find(like => like === (user?.result?.googleId || user?.result?._id))
-	const hasLikedPost = likes.find(like => like === userId)
 	const avatar = false
-	const [isDialogOpen, setIsDialogOpen] = useState(false)
-	const history = useHistory()
-	const [likesMock, setLikesMock] = useState(likes)
 
 	const handleLike = async () => {
 		dispatch(likePost(_id))
 
-		if (hasLikedPost) {
+		if (isUserLike) {
 			setLikesMock(likes.filter(id => id !== userId))
 		} else {
 			setLikesMock([...likes, userId])
@@ -55,21 +53,22 @@ const Post = ({
 	}
 
 	return (
-		<Stack bg={bg} borderRadius="lg" direction="column" p="8" spacing={4} w="100%">
-			{selectedFile && (
-				<Image
-					alt={title}
-					borderRadius="lg"
-					boxSize="250"
-					fallback={<Skeleton borderRadius="lg" h="250" />}
-					fit="cover"
-					loading="lazy"
-					src={selectedFile}
-					w="100%"
-				/>
-			)}
-			<Stack direction="row" spacing="4">
-				<Flex direction="column">
+		<Stack
+			bg={bg}
+			borderRadius="lg"
+			direction={{
+				sm: "column-reverse",
+				md: "column-reverse",
+				lg: "column-reverse",
+				xl: "row",
+			}}
+			h="100%"
+			p="8"
+			spacing={4}
+			w="100%"
+		>
+			<Stack h="100%" spacing="4" w="100%">
+				<Stack direction="row" spacing="2">
 					{avatar && (
 						<Image
 							alt={user?.result?.name}
@@ -81,69 +80,92 @@ const Post = ({
 							src={avatar}
 						/>
 					)}
-				</Flex>
-				<Flex direction="column">
-					<Text fontSize="lg">{name}</Text>
-					<Text fontSize="sm">
-						{formatDistance(new Date(), createdAt ? new Date(createdAt) : new Date()) +
-							" ago"}
+					<Stack direction="column">
+						<Text fontSize="lg">{name}</Text>
+						<Text fontSize="sm">
+							{formatDistance(new Date(), createdAt ? new Date(createdAt) : new Date()) +
+								" ago"}
+						</Text>
+					</Stack>
+				</Stack>
+
+				<Text
+					_hover={{ cursor: "pointer" }}
+					fontSize="3xl"
+					marginBottom="2"
+					onClick={openPost}
+				>
+					{title}
+				</Text>
+
+				<Flex grow="1">
+					<Text fontSize="md" noOfLines={[2, 4, 6]}>
+						{message}
 					</Text>
 				</Flex>
-			</Stack>
-			<Text
-				_hover={{ cursor: "pointer", textDecoration: "underline" }}
-				fontSize="3xl"
-				marginBottom="2"
-				onClick={openPost}
-			>
-				{title}
-			</Text>
-			<Text fontSize="md" noOfLines={[2, 4, 6]}>
-				{message}
-			</Text>
-			<Stack direction="row" overflow="auto" spacing="2">
-				{[...new Set(tags)]
-					.filter(e => e)
-					.map(tag => (
-						<Badge key={getRandomId()} bg="primary.400" color="white">
-							{tag}
-						</Badge>
-					))}
-			</Stack>
-			<Stack direction="row" spacing="4">
-				<Button
-					colorScheme="primary"
-					disabled={!user?.result}
-					size="sm"
-					variant={isUserLike ? "ghost" : "outline"}
-					onClick={handleLike}
-				>
-					<Likes isUserLike={isUserLike} likes={likesMock} />
-				</Button>
-				{isPostCreator && (
+
+				<Stack direction="row" overflow="auto" spacing="2">
+					{[...new Set(tags)]
+						.filter(e => e)
+						.map(tag => (
+							<Badge key={getRandomId()} bg="primary.400" color="white">
+								{tag}
+							</Badge>
+						))}
+				</Stack>
+
+				<Stack direction="row" spacing="4">
 					<Button
 						colorScheme="primary"
-						leftIcon={<FaPen />}
+						disabled={!user?.result}
 						size="sm"
-						variant="outline"
-						onClick={() => setCurrentId(_id)}
+						variant={isUserLike ? "ghost" : "outline"}
+						onClick={handleLike}
 					>
-						Edit
+						<Likes isUserLike={isUserLike} likes={likesMock} />
 					</Button>
-				)}
-				{isPostCreator && (
-					<Button
-						bg={bgDelete}
-						colorScheme="primary"
-						leftIcon={<FaEraser />}
-						size="sm"
-						variant="solid"
-						onClick={() => setIsDialogOpen(true)}
-					>
-						Delete
-					</Button>
-				)}
+					{isPostCreator && (
+						<Button
+							colorScheme="primary"
+							leftIcon={<FaPen />}
+							size="sm"
+							variant="outline"
+							onClick={() => setCurrentId(_id)}
+						>
+							Edit
+						</Button>
+					)}
+					{isPostCreator && (
+						<Button
+							bg={bgDelete}
+							colorScheme="primary"
+							leftIcon={<FaEraser />}
+							size="sm"
+							variant="solid"
+							onClick={() => setIsDialogOpen(true)}
+						>
+							Delete
+						</Button>
+					)}
+				</Stack>
 			</Stack>
+
+			{selectedFile && (
+				<Image
+					_hover={{ cursor: "pointer" }}
+					alt={title}
+					borderRadius="lg"
+					fallback={<Skeleton borderRadius="lg" h="400" />}
+					fit="cover"
+					loading="lazy"
+					maxHeight="400px"
+					maxWidth="600px"
+					src={selectedFile}
+					w="100%"
+					onClick={openPost}
+				/>
+			)}
+
 			<Dialog
 				_id={_id}
 				action={() => dispatch(deletePost(_id))}
