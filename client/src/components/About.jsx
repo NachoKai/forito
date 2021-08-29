dc31ed187a6098cb85e1134d8c9ed7081f853826
Nacho Caiafa <ignacio.caiafa@gmail.com>
2021-08-28 22:17:37 -0300

    add footer

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,28 +1,32 @@
-import { Button, Flex, Text } from "@chakra-ui/react"
+import { Button, Flex, Stack, Text } from "@chakra-ui/react"
 import { FaGithub, FaHandSpock } from "react-icons/fa"
 import { Link } from "react-router-dom"
 
 const About = () => {
 	return (
 		<Flex flexGrow align="center" direction="column" h="100%" justify="center" p="8">
-			<Text color="primary.400" fontSize="6xl">
-				<FaHandSpock />
-			</Text>
-			<Text color="primary.400" fontSize="6xl">
-				Hi, I&apos;m Nacho!
-			</Text>
-			<Text color="primary.400" fontSize="3xl">
-				And this is Forito✨
-			</Text>
-			<Button colorScheme="primary" leftIcon={<FaGithub />} variant="outline">
-				<Link
-					fontSize="xl"
-					target="_blank"
-					to={{ pathname: "https://github.com/NachoKai/forito" }}
-				>
-					Github
-				</Link>
-			</Button>
+			<Stack spacing="4">
+				<Flex justify="center">
+					<Text color="primary.400" fontSize="6xl">
+						<FaHandSpock />
+					</Text>
+				</Flex>
+				<Text color="primary.400" fontSize="6xl">
+					Hi, I&apos;m Nacho!
+				</Text>
+				<Text color="primary.400" fontSize="3xl">
+					And this is Forito✨
+				</Text>
+				<Button colorScheme="primary" leftIcon={<FaGithub />} variant="outline">
+					<Link
+						fontSize="xl"
+						target="_blank"
+						to={{ pathname: "https://github.com/NachoKai/forito" }}
+					>
+						Github
+					</Link>
+				</Button>
+			</Stack>
 		</Flex>
 	)
 }
