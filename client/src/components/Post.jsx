3dc0dbf260a768c1c2659c79a5affa04721c07ee
Nacho Caiafa <ignacio.caiafa@gmail.com>
2021-12-08 18:00:55 -0300

    update dependencies

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -36,7 +36,7 @@ const Post = ({
 		selectedFile,
 		comments,
 	},
-	handleClick,
+	handleScroll,
 }) => {
 	const dispatch = useDispatch()
 	const history = useHistory()
@@ -81,8 +81,8 @@ const Post = ({
 
 	const handleEdit = useCallback(() => {
 		setCurrentId(_id)
-		handleClick()
-	}, [_id, handleClick, setCurrentId])
+		handleScroll()
+	}, [_id, handleScroll, setCurrentId])
 
 	return (
 		<Stack
