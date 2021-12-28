10966711d7d320bb85a7565e0af27a7b59cdbce3
Nacho Caiafa <ignacio.caiafa@gmail.com>
2021-12-28 15:58:07 -0300

    add tests

---
+++ client/src/tests/About.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render, screen } from './test-utils'
+import About from '../components/About'
+
+describe('About', () => {
+	test('Renders component sections', () => {
+		render(<About />)
+		const title = screen.getByRole('heading', { name: /hi, i'm nacho!/i })
+		const subTitle = screen.getByRole('heading', { name: /and this is forito ✨/i })
+		const button = screen.getByRole('button', { name: /github/i })
+
+		expect(title).toBeInTheDocument()
+		expect(subTitle).toBeInTheDocument()
+		expect(button).toBeInTheDocument()
+	})
+})
--- client/src/tests/Navbar.test.js
+++ client/src/tests/Navbar.test.js
@@ -6,7 +6,11 @@ describe('Navbar', () => {
 	test('Renders title', () => {
 		render(<Navbar />)
 		const title = screen.getByRole('link', { name: /forito ✨/i })
+		const themeButton = screen.getByRole('button', { name: /light|dark mode/i })
+		const asd = screen.getByRole('button', { name: /login|logout/i })
 
 		expect(title).toBeInTheDocument()
+		expect(themeButton).toBeInTheDocument()
+		expect(asd).toBeInTheDocument()
 	})
 })
