78dd4cdc4918845b27ac420736961c6b73de94f6
Nacho Caiafa <ignacio.caiafa@gmail.com>
2022-06-05 15:46:45 -0300

    refactor createRoot element

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import * as ReactDOMClient from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 
 import './index.css'
 import store from './redux/store'
-import App from './App.jsx'
 import theme from './theme'
+import App from './App.jsx'
 import { LoadingContextProvider } from './components/common/LoadingScreen/LoadingContext'
 
-const container = document.getElementById('root')
-const root = ReactDOMClient.createRoot(container)
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
 
 root.render(
 	<Provider store={store}>
 		<ChakraProvider theme={theme}>
 			<ColorModeScript initialColorMode={theme?.config?.initialColorMode} />
 			<LoadingContextProvider>
-				<App />
+				<StrictMode>
+					<App />
+				</StrictMode>
 			</LoadingContextProvider>
 		</ChakraProvider>
 	</Provider>
