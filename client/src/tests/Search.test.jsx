2ad788c739b66d12d12d2bb56988e8f87a9546a0
Nacho Caiafa <ignacio.caiafa@gmail.com>
2022-08-06 00:31:07 -0300

    add ColorPicker test

--- client/src/tests/Search.test.jsx
+++ client/src/tests/Search.test.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 
-import Search from '../components/Search'
+import ColorPicker from '../components/ColorPicker'
 import { render, screen } from './test-utils'
 
-describe('Search', () => {
+describe('ColorPicker', () => {
 	test('Renders component sections', () => {
-		render(<Search />)
-		const searchButton = screen.getByRole('button', { name: /search/i })
+		render(<ColorPicker />)
+		const selectedColor = screen.getByText(/blue\.500/i)
+		const closeButton = screen.getByLabelText(/close/i)
+		const redButton = screen.getByLabelText(/red\.500/i)
+		const orangeButton = screen.getByLabelText(/orange\.400/i)
+		const yellowButton = screen.getByLabelText(/yellow\.300/i)
+		const greenButton = screen.getByLabelText(/green\.500/i)
+		const tealButton = screen.getByLabelText(/teal\.300/i)
+		const cyanButton = screen.getByLabelText(/cyan\.400/i)
+		const purpleButton = screen.getByLabelText(/purple\.500/i)
+		const pinkButton = screen.getByLabelText(/pink\.400/i)
+		const grayButton = screen.getByLabelText(/gray\.500/i)
 
-		expect(searchButton).toBeInTheDocument()
+		expect(selectedColor).toBeInTheDocument()
+		expect(closeButton).toBeInTheDocument()
+		expect(redButton).toBeInTheDocument()
+		expect(orangeButton).toBeInTheDocument()
+		expect(yellowButton).toBeInTheDocument()
+		expect(greenButton).toBeInTheDocument()
+		expect(tealButton).toBeInTheDocument()
+		expect(cyanButton).toBeInTheDocument()
+		expect(purpleButton).toBeInTheDocument()
+		expect(pinkButton).toBeInTheDocument()
+		expect(grayButton).toBeInTheDocument()
 	})
 })
