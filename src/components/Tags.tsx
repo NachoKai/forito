c7832b603116938254349bd2a69fdf70f1dd7b26
Nacho Caiafa <ignacio.caiafa@gmail.com>
2022-11-05 23:02:04 -0300

    change tags view with react query

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,42 +1,45 @@
 import { Divider, Flex, Heading, Stack, Text } from '@chakra-ui/react'
-import { useEffect, useMemo } from 'react'
+import { useMemo } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import { useParams } from 'react-router-dom'
 
-import { usePostsStore } from '../state/postsStore'
+import { usePostsBySearch } from '../hooks/data/posts'
 import { CreateGradColor } from '../theme'
 import { PostI, UserI } from '../types'
 import { checkIsAdmin } from '../utils/checkIsAdmin'
 import { checkIsPostCreator } from '../utils/checkIsPostCreator'
 import { getUserLocalStorage } from '../utils/getUserLocalStorage'
+import { Loading } from './common/Loading'
 import { StaggeredSlideFade } from './common/StaggeredSlideFade'
 import { Post } from './Post'
 
 const Tags: React.FC = () => {
 	const { name } = useParams()
-	const { posts, loading, getPostsBySearch } = usePostsStore()
+	const searchQuery = { tags: name }
+	const { postsBySearch, isLoading, isSuccess } = usePostsBySearch(searchQuery)
 	const user: UserI = getUserLocalStorage()
 	const userEmail = user?.result?.email
 	const isAdmin = checkIsAdmin(userEmail)
-	const title = posts?.length !== 1 ? `${posts?.length} Posts` : `${posts?.length} Post`
+	const title =
+		postsBySearch?.length === 1
+			? `${postsBySearch?.length} Post`
+			: `${postsBySearch?.length} Posts`
 
 	const publicPosts = useMemo(
 		() =>
-			posts?.filter((post: PostI) => {
+			postsBySearch?.filter((post: PostI) => {
 				const isPrivate = post?.privacy === 'private'
 				const creator = post?.creator
 				const isPostCreator = checkIsPostCreator(user, creator)
 
 				return !isPrivate || (isPrivate && isPostCreator) || isAdmin
 			}),
-		[isAdmin, posts, user]
+		[isAdmin, postsBySearch, user]
 	)
 
-	useEffect(() => {
-		getPostsBySearch({ tags: name })
-	}, [getPostsBySearch, name])
-
-	return !publicPosts?.length && !loading ? (
+	return isLoading ? (
+		<Loading />
+	) : !publicPosts?.length && isSuccess ? (
 		<Flex align='center' direction='column' h='100%' minH='100vh' my='64px'>
 			<Text color='primary.400' fontSize='6xl' mb='16px'>
 				<FaSearch />
@@ -61,13 +64,13 @@ const Tags: React.FC = () => {
 		>
 			<Stack spacing='2'>
 				<Text fontSize='2xl'>#{name?.toUpperCase()}</Text>
-				<Text fontSize='md'>{!loading && posts?.length ? title : ''}</Text>
+				<Text fontSize='md'>{isSuccess && postsBySearch?.length ? title : ''}</Text>
 			</Stack>
 
 			<Divider />
 
 			<StaggeredSlideFade spacing='3'>
-				{posts?.map(post => (
+				{postsBySearch?.map(post => (
 					<Stack key={post._id}>
 						<Post post={post} />
 					</Stack>
