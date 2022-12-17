452ef39d178769433d26d37e874e4543aab76c95
Nacho Caiafa <ignacio.caiafa@gmail.com>
2022-12-17 17:16:54 -0300

    refactor loading

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,5 +1,5 @@
 import { Spinner, useColorMode } from '@chakra-ui/react'
-import React from 'react'
+import styled from 'styled-components'
 
 import { getThemeColor } from '../../utils/getThemeColor.ts'
 
@@ -7,21 +7,54 @@ export const Loading = () => {
 	const { colorMode } = useColorMode()
 
 	return (
-		<div
-			className={
-				colorMode === 'light' ? 'loading-background-light' : 'loading-background-dark'
-			}
-		>
-			<div className='loading-bar'>
-				<Spinner
-					className='loading-circle'
-					color={() => `${getThemeColor()}.500`}
-					emptyColor='gray.200'
-					size='xl'
-					speed='0.65s'
-					thickness='8px'
-				/>
+		<Container>
+			<div
+				className={
+					colorMode === 'light' ? 'loading-background-light' : 'loading-background-dark'
+				}
+			>
+				<div className='loading-bar'>
+					<Spinner
+						className='loading-circle'
+						color={() => `${getThemeColor()}.500`}
+						emptyColor='gray.200'
+						size='xl'
+						speed='0.65s'
+						thickness='8px'
+					/>
+				</div>
 			</div>
-		</div>
+		</Container>
 	)
 }
+
+const Container = styled.div`
+	pointer-events: none;
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: 9998;
+	width: 100vw;
+	height: 100vh;
+	pointer-events: none;
+	backdrop-filter: blur(15px);
+
+	.loading-background-light {
+		pointer-events: none;
+		background: rgb(255 255 255 / 40%);
+	}
+
+	.loading-background-dark {
+		pointer-events: none;
+		background: rgb(0 0 0 / 40%);
+	}
+
+	.loading-bar .loading-circle {
+		z-index: 9999;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		pointer-events: none;
+		cursor: progress;
+	}
+`
--- src/components/common/LoadingScreen/LoadingScreen.tsx
+++ src/components/common/LoadingScreen/LoadingScreen.tsx
@@ -1,17 +1,13 @@
-import { Spinner, useColorMode } from '@chakra-ui/react'
-import { useContext, useEffect, useLayoutEffect, useRef } from 'react'
+import { useContext, useLayoutEffect } from 'react'
 
 import { usePostsStore } from '../../../state/postsStore'
-import { getThemeColor } from '../../../utils/getThemeColor'
+import { Loading } from '../Loading'
 import { LoadingContext } from './LoadingContext'
 
 const LoadingScreen = () => {
 	const { isLoading } = useContext(LoadingContext)
-	const { colorMode } = useColorMode()
 	const { loading } = usePostsStore()
-	const loadingRef = useRef(null)
 	const { showLoading, hideLoading } = useContext(LoadingContext)
-	const color = `${getThemeColor()}.500`
 
 	useLayoutEffect(() => {
 		if (isLoading) {
@@ -21,29 +17,7 @@ const LoadingScreen = () => {
 		}
 	}, [isLoading, showLoading, hideLoading])
 
-	useEffect(() => {
-		if (loading) loadingRef.current.focus()
-	}, [loading])
-
-	return loading ? (
-		<div
-			ref={loadingRef}
-			className={
-				colorMode === 'light' ? 'loading-background-light' : 'loading-background-dark'
-			}
-		>
-			<div className='loading-bar'>
-				<Spinner
-					className='loading-circle'
-					color={color}
-					emptyColor='gray.200'
-					size='xl'
-					speed='0.65s'
-					thickness='8px'
-				/>
-			</div>
-		</div>
-	) : null
+	return loading ? <Loading /> : null
 }
 
 export default LoadingScreen
