31d805193e0bccc4bbf60860827e8d0c42d13352
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-01-01 12:43:12 -0300

    add fonts to index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/roboto'
+import '@fontsource/roboto-slab'
 
 import { App } from './App'
 import { LoadingContextProvider } from './components/common/LoadingScreen/LoadingContext'
